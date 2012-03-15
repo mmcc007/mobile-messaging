6e4f0fec3dffa92cb867f37e4f38c8bc9434477b
Maurice <Maurice@Orbsoft>
2012-03-14 22:14:45 -0700

    Create the spring security authorization object after returning from GitKit

--- Roadz/grails-app/services/com/orbsoft/roadz/service/AccountService.groovy
+++ Roadz/grails-app/services/com/orbsoft/roadz/service/AccountService.groovy
@@ -1,9 +1,16 @@
 package com.orbsoft.roadz.service
 
+import java.util.HashSet;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthorityImpl;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
 import com.google.apps.easyconnect.easyrp.client.basic.data.Account;
 import com.google.apps.easyconnect.easyrp.client.basic.data.AccountException;
 import com.orbsoft.roadz.domain.User
@@ -61,7 +68,8 @@ class AccountService implements com.google.apps.easyconnect.easyrp.client.basic.
 	        user.setPhotoUrl("http://www.google.com/uds/modules/identitytoolkit/image/nophoto.png");
 	      }
 	      user.setFederated(true);
-		  user.save()
+		  user.save();
+		  setAuth(user.getUsername());
 	      return user;
 	    } catch (JSONException e) {
 	      throw new AccountException(AccountException.UNKNOWN_ERROR);
@@ -70,15 +78,29 @@ class AccountService implements com.google.apps.easyconnect.easyrp.client.basic.
 
 	@Override
 	public Account getAccountByEmail(String email) {
-		return User.findByUsername(email)
+		Account account = User.findByUsername(email)
+		if (account != null) setAuth(email)
+		return account
 	}
 
 	@Override
 	public void toFederated(String email) throws AccountException {
-    User user = User.findByEmail(email);
+    User user = User.findByUsername(email);
     if (user == null) {
       throw new AccountException(AccountException.ACCOUNT_NOT_FOUND);
     }
     user.setFederated(true);
+	}
+	
+	/**
+	 * Set authentication for Spring Security 
+	 * @param principal
+	 */
+	private void setAuth(String principal) {
+		GrantedAuthority grantedAuthority = new GrantedAuthorityImpl("ROLE_USER");
+		HashSet<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+		authorities.add(grantedAuthority);
+		Authentication anAuthentication = new PreAuthenticatedAuthenticationToken(principal, "no credentials",authorities);
+		SecurityContextHolder.getContext().setAuthentication(anAuthentication);
 	}
 }
