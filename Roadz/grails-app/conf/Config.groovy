712f7cf02633d8004e5bf600c432f3d2b537ba74
Maurice McCabe <mmccabe@orbsoft.com>
2012-05-17 20:43:38 -0700

    External over-rides via properties file

--- Roadz/grails-app/conf/Config.groovy
+++ Roadz/grails-app/conf/Config.groovy
@@ -1,10 +1,11 @@
 // locations to search for config files that get merged into the main config
 // config files can either be Java properties files or ConfigSlurper scripts
 
-// grails.config.locations = [ "classpath:${appName}-config.properties",
-//                             "classpath:${appName}-config.groovy",
-//                             "file:${userHome}/.grails/${appName}-config.properties",
-//                             "file:${userHome}/.grails/${appName}-config.groovy"]
+grails.config.locations = [
+    "classpath:${appName}-config.properties",
+    "classpath:${appName}-config.groovy",
+    "file:${userHome}/.grails/${appName}-config.properties",
+    "file:${userHome}/.grails/${appName}-config.groovy" ]
 
 // if (System.properties["${appName}.config.location"]) {
 //    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
@@ -102,8 +103,8 @@ log4j = {
 
 //grails.plugin.inviter.facebook.key='197755966948551'
 //grails.plugin.inviter.facebook.secret='5dc5ace6405e5882511e6dd177aa5f77'
-grails.plugin.inviter.facebook.key='106080722853795'
-grails.plugin.inviter.facebook.secret='d5f1cbe4ce5799bf4452b2b5a5787774'
+//grails.plugin.inviter.facebook.key='106080722853795'
+//grails.plugin.inviter.facebook.secret='0f35f877ef70901985193824676ff096'
 
 grails.plugin.inviter.yahoo.key='dj0yJmk9T21LU3dnazNxUnJ2JmQ9WVdrOWRGQlBhWFZ4TlRZbWNHbzlNVEV6TkRreU5qUTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD04Nw--'
 grails.plugin.inviter.yahoo.secret= 'a21f29aa15b65c2394a5cd18a5f266eba15fcc91'
@@ -114,8 +115,8 @@ grails.plugin.inviter.linkedin.secret='KBtxC418sdmu5eur'
 
 //grails.plugin.inviter.twitter.key='G8rCU7AHBsZAbeLUAP0i7Q'
 //grails.plugin.inviter.twitter.secret='BKbt1ygWu0q4xsLuJ8bhjYvegojVHz2GXn1Z5leoXN8'
-grails.plugin.inviter.twitter.key='hEa3ktzqOC47av7haygA'
-grails.plugin.inviter.twitter.secret='MHukMsTmPnJ6bzhChKqVO3iLsWs2uBbFRraYR8WC1w'
+//grails.plugin.inviter.twitter.key='hEa3ktzqOC47av7haygA'
+//grails.plugin.inviter.twitter.secret='MHukMsTmPnJ6bzhChKqVO3iLsWs2uBbFRraYR8WC1w'
 
 grails.plugin.inviter.windowslive.key='0000000040062703'
 grails.plugin.inviter.windowslive.secret='uuQUGpig3kbv14SdlGwPdHYS86eVWlRc'
