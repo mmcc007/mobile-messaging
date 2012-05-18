099df183d602329490299ce682bc8cdda27ca16c
Maurice McCabe <mmccabe@orbsoft.com>
2012-05-17 20:46:50 -0700

    Cleanup

--- Roadz/grails-app/controllers/com/orbsoft/roadz/controller/UserController.groovy
+++ Roadz/grails-app/controllers/com/orbsoft/roadz/controller/UserController.groovy
@@ -22,27 +22,6 @@ class UserController {
 		User user = User.findByUsername(username) 
 		def friendship = Friendship.findWhere(friendedBy: user, status: Friendship._INVITE_FRIENDS)
 
-		//def chatService = grailsApplication.mainContext.getBean("chatService")
-
-		//println "friendsjson: grailsApplication.mainContext = " + grailsApplication.mainContext
-//		println "friendsjson: myChatService.serviceMethod() = " + myChatService.serviceMethod()
-//		println "friendsjson: myBean.someProperty = " + myBean.someProperty
-//		println "friendsjson: myBean.otherProperty = " + myBean.otherProperty
-//            Map<String, Object> chat = new HashMap<String, Object>();
-//            String text="ServerMessage message";
-//            chat.put("chat", text);
-//            chat.put("timestamp", "Sat, 12 May 2012 14:02:11 GMT");
-//            chat.put("user", "mmcc007@gmail.com");
-//            chat.put("peer", "mmcc007@gmail.com");
-//            chat.put("room", "/chat/demo");
-//            chat.put("scope", "private");
-//            ServerMessage.Mutable forward = bayeux.newMessage();
-//            forward.setChannel("/service/privatechat");
-//            forward.setId("123");
-//            forward.setData(chat);
-//        println "forward=" + forward
-//		myChatService.privateChat(myChatService.getClient('demo', 'mmcc007@gmail.com'), forward)
-
 		//userService.sendMsg(username, "ServerMessage message")
 
 		if (friendship) 
